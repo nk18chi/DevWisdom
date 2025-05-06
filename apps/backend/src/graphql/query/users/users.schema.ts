3ec335282ebb83ba871ec2e270bc6d64be492836
Naoki Mita <naoki.mita18@gmail.com>
2025-05-05 17:51:24 -0700

    refactor(backend): remove user schema type from users.schema
    it is already defined in user.schema

--- apps/backend/src/graphql/query/users/users.schema.ts
+++ apps/backend/src/graphql/query/users/users.schema.ts
@@ -1,12 +1,4 @@
 const usersTypeDef = `#graphql
-type User {
-  _id: ID!
-  email: String!
-  emailVerified: Boolean!
-  status: UserStatus!
-  password: String! # don't expose password so this is always null, handling by GraphQL Shield
-}
-
 type UserConnection @cacheControl(maxAge: 60) {
   edges: [UserEdge]
   pageInfo: PageInfo
