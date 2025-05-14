c440e7cb1b5c95c61014e999f359d65dedfd54a3
Naoki Mita <naoki.mita18@gmail.com>
2025-05-13 18:04:18 -0700

    feat(backend): add timestamp for user mongo schema

--- apps/backend/src/repositories/user/User.schema.ts
+++ apps/backend/src/repositories/user/User.schema.ts
@@ -2,12 +2,15 @@ import { model, Schema } from 'mongoose';
 import { GqlUserStatus } from '../../graphql/types';
 import { User } from '../../entities/User.entity';
 
-const userSchema = new Schema<User>({
-  email: { type: String, required: true, unique: true },
-  emailVerified: { type: Boolean, required: true, default: false },
-  password: { type: String, required: true },
-  status: { type: String, required: true, enum: GqlUserStatus, default: GqlUserStatus.Active },
-});
+const userSchema = new Schema<User>(
+  {
+    email: { type: String, required: true, unique: true },
+    emailVerified: { type: Boolean, required: true, default: false },
+    password: { type: String, required: true },
+    status: { type: String, required: true, enum: GqlUserStatus, default: GqlUserStatus.Active },
+  },
+  { timestamps: true },
+);
 
 const UserModel = model<User>('User', userSchema);
 
