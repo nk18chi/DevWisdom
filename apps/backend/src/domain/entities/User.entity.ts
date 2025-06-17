a8c2e06db2cd34b1182ebfd1452d64a602d5c3e3
Naoki Mita <naoki.mita18@gmail.com>
2025-06-17 08:56:22 -0700

    refactor(backend): undo validatedUserWorkflow
    allow admin user to view any user

--- apps/backend/src/domain/entities/User.entity.ts
+++ apps/backend/src/domain/entities/User.entity.ts
@@ -11,14 +11,6 @@ interface User {
   password: HashingPassword;
 }
 
-interface ValidatedUser {
-  _id: MongoId;
-  email: Email;
-  emailVerified: true;
-  status: GqlUserStatus.Active;
-  password: HashingPassword;
-}
-
 interface CreatedUser {
   _id: MongoId;
   email: Email;
@@ -43,5 +35,5 @@ interface AuthorizedUser {
   _id: MongoId;
 }
 
-export { CreatedUser, UpdatedUser, User, AuthorizedUser, SignedInUser, ValidatedUser };
+export { CreatedUser, UpdatedUser, User, AuthorizedUser, SignedInUser };
 export default User;
--- apps/backend/src/graphql/query/quote/quote.resolver.ts
+++ apps/backend/src/graphql/query/quote/quote.resolver.ts
@@ -1,8 +1,6 @@
-import { ok } from 'neverthrow';
 import { DEFAULT_PAGE_SIZE } from '../../../config/constants';
-import User, { ValidatedUser } from '../../../domain/entities/User.entity';
+import User from '../../../domain/entities/User.entity';
 import QuoteModel from '../../../infrastructure/repositories/quote/Quote.schema';
-import validatedUserWorkflow from '../../../workflows/validatedUser/validatedUser.workflows';
 import { GqlResolvers } from '../../types';
 import { Types } from 'mongoose';
 
@@ -11,18 +9,11 @@ const quotesResolver: GqlResolvers = {
     quote: async (_, { id }) => QuoteModel.findById(id).lean(),
   },
   Quote: {
-    user: async (quote, _, context): Promise<ValidatedUser> => {
+    user: async (quote, _, context) => {
       const { userDataLoader } = context.dataLoaders;
       const user = await userDataLoader.load(new Types.ObjectId(quote.userId.toString()));
       if (!user) throw new Error('User not found');
-      const workflow = validatedUserWorkflow;
-      const result = ok(user).andThen(workflow);
-      return result.match(
-        (validatedUser) => validatedUser,
-        (error) => {
-          throw error;
-        },
-      );
+      return user;
     },
     likeCount: async (quote) => quote.likes.length,
     likedUsers: async (quote, { number }, context) => {
